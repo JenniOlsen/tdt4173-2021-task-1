90e2b944d64886600cc2e9a72ed60edf37b6f628
JenniOlsen <jennisolsen@gmail.com>
2021-09-17 14:08:50 +0200

    completed decision tree
    except for .get_rules() method

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -1,15 +1,32 @@
-import numpy as np 
+from collections import Counter
+import numpy as np
+from numpy.core.defchararray import array, count 
 import pandas as pd 
+
+
+#from collections import Counter
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, *, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+    
+    def is_leaf_node(self):
+        return self.value is not None
 
 
 class DecisionTree:
     
-    def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
+    def __init__(self, min_samples_split=2, max_depth=100, n_feats=None):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.n_feats = n_feats
+        self.root = None
     
     def fit(self, X, y):
         """
@@ -22,8 +39,67 @@ class DecisionTree:
             y (pd.Series): a vector of discrete ground-truth labels
         """
         # TODO: Implement 
-        raise NotImplementedError()
+        #grow tree
+        self.n_feats = X.shape[1] if not self.n_feats else min(self.n_feats, X.shape[1])
+        self.root = self._grow_tree(X,y)
+        #raise NotImplementedError()
     
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        #stopping criteria
+        if (depth >= self.max_depth
+            or n_labels == 1
+            or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, self.n_feats, replace=False)
+
+        #greedy search
+        best_feat, best_thresh = self._best_criteria(X, y, feat_idxs)
+        left_idxs, right_idxs = self._split(X[:,best_feat], best_thresh)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
+        return Node(best_feat, best_thresh, left, right)
+    
+    def _best_criteria(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_threh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:,feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_threh = threshold
+        return split_idx,split_threh
+
+    def _information_gain(self, y, X_column, split_threh):
+        #parent E
+        parent_entropy = formatEntropy(y)
+        #genereate split
+        left_idxs, right_idxs = self._split(X_column, split_threh)
+        #weighted avg child E
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = formatEntropy(y[left_idxs]), formatEntropy(y[right_idxs])
+        child_entropy = (n_l/n)* e_l +(n_r/n) * e_r
+
+        #return info gain
+        ig = parent_entropy - child_entropy
+        return ig
+
+
+    def _split(self, X_column, split_threh):
+        left_idxs = np.argwhere(X_column <= split_threh).flatten()
+        right_idxs = np.argwhere(X_column > split_threh).flatten()
+        return left_idxs, right_idxs
+
     def predict(self, X):
         """
         Generates predictions
@@ -39,8 +115,22 @@ class DecisionTree:
             A length m vector with predictions
         """
         # TODO: Implement 
-        raise NotImplementedError()
+        return np.array([self._traverse_tree(x, self.root) for x in X])
+        #raise NotImplementedError()
     
+    def _traverse_tree(self, x, node):
+        if node.is_leaf_node():
+            return node.value
+        
+        if x[node.feature] <= node.threshold:
+            return self._traverse_tree(x, node.left)
+        return self._traverse_tree(x, node.right)
+    
+    def _most_common_label(Self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
+
     def get_rules(self):
         """
         Returns the decision tree as a list of rules
@@ -60,7 +150,8 @@ class DecisionTree:
         ]
         """
         # TODO: Implement
-        raise NotImplementedError()
+        
+        #raise NotImplementedError()
 
 
 # --- Some utility functions 
@@ -78,6 +169,18 @@ def accuracy(y_true, y_pred):
     """
     assert y_true.shape == y_pred.shape
     return (y_true == y_pred).mean()
+
+def formatEntropy(y):
+    count1 = 0
+    count2 = 0
+    testArray = np.array(y)
+    for i in y:
+        if (i == "Yes" or i == "success"):
+            count1 += 1
+        else:
+            count2 += 1
+    arrayTemp = np.array([count1, count2])
+    return entropy(arrayTemp)
 
 
 def entropy(counts):
@@ -97,10 +200,10 @@ def entropy(counts):
         of the partitioning.
     
     """
+
     assert (counts >= 0).all()
     probs = counts / counts.sum()
     probs = probs[probs > 0]  # Avoid log(0)
     return - np.sum(probs * np.log2(probs))
-
 
 
--- k_means/k_means.py
+++ k_means/k_means.py
@@ -1,47 +1,65 @@
+from os import replace
+from matplotlib import colors
 import numpy as np 
 import pandas as pd 
+import matplotlib.pyplot as plt 
 # IMPORTANT: DO NOT USE ANY OTHER 3RD PARTY PACKAGES
 # (math, random, collections, functools, etc. are perfectly fine)
 
 
 class KMeans:
+    def __init__(self, k=2, tol=0.001, max_iter=300):
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def fit(self,data):
+
+        self.centroids = {}
+
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+
+        for i in range(self.max_iter):
+            self.classifications = {}
+
+            for i in range(self.k):
+                self.classifications[i] = []
+
+            for featureset in data:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+
+            optimized = True
+
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    print(np.sum((current_centroid-original_centroid)/original_centroid*100.0))
+                    optimized = False
+
+            if optimized:
+                break
+
+    def predict(self,data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
     
-    def __init__(self):
-        # NOTE: Feel free add any hyperparameters 
-        # (with defaults) as you see fit
-        pass
-        
-    def fit(self, X):
-        """
-        Estimates parameters for the classifier
-        
-        Args:
-            X (array<m,n>): a matrix of floats with
-                m rows (#samples) and n columns (#features)
-        """
-        # TODO: Implement
-        raise NotImplementedError()
-    
-    def predict(self, X):
-        """
-        Generates predictions
-        
-        Note: should be called after .fit()
-        
-        Args:
-            X (array<m,n>): a matrix of floats with 
-                m rows (#samples) and n columns (#features)
-            
-        Returns:
-            A length m integer array with cluster assignments
-            for each point. E.g., if X is a 10xn matrix and 
-            there are 3 clusters, then a possible assignment
-            could be: array([2, 0, 0, 1, 2, 1, 1, 0, 2, 2])
-        """
-        # TODO: Implement 
-        raise NotImplementedError()
-    
-    def get_centroids(self):
+    def _get_centroids(self, clusters):
+        # assign mean value of clusters to centroids
+        centroids = np.zeros((self.K, self.n_features))
+        for cluster_idx, cluster in enumerate(clusters):
+            cluster_mean = np.mean(self.X[cluster], axis=0)
+            centroids[cluster_idx] = cluster_mean
+        return centroids
         """
         Returns the centroids found by the K-mean algorithm
         
@@ -57,109 +75,5 @@ class KMeans:
         ])
         """
         # TODO: Implement 
-        raise NotImplementedError()
-    
-    
-    
-    
-# --- Some utility functions 
+        #raise NotImplementedError()
 
-
-def euclidean_distortion(X, z):
-    """
-    Computes the Euclidean K-means distortion
-    
-    Args:
-        X (array<m,n>): m x n float matrix with datapoints 
-        z (array<m>): m-length integer vector of cluster assignments
-    
-    Returns:
-        A scalar float with the raw distortion measure 
-    """
-    X, z = np.asarray(X), np.asarray(z)
-    assert len(X.shape) == 2
-    assert len(z.shape) == 1
-    assert X.shape[0] == z.shape[0]
-    
-    distortion = 0.0
-    for c in np.unique(z):
-        Xc = X[z == c]
-        mu = Xc.mean(axis=0)
-        distortion += ((Xc - mu) ** 2).sum()
-        
-    return distortion
-
-def euclidean_distance(x, y):
-    """
-    Computes euclidean distance between two sets of points 
-    
-    Note: by passing "y=0.0", it will compute the euclidean norm
-    
-    Args:
-        x, y (array<...,n>): float tensors with pairs of 
-            n-dimensional points 
-            
-    Returns:
-        A float array of shape <...> with the pairwise distances
-        of each x and y point
-    """
-    return np.linalg.norm(x - y, ord=2, axis=-1)
-
-def cross_euclidean_distance(x, y=None):
-    """
-    Compute Euclidean distance between two sets of points 
-    
-    Args:
-        x (array<m,d>): float tensor with pairs of 
-            n-dimensional points. 
-        y (array<n,d>): float tensor with pairs of 
-            n-dimensional points. Uses y=x if y is not given.
-            
-    Returns:
-        A float array of shape <m,n> with the euclidean distances
-        from all the points in x to all the points in y
-    """
-    y = x if y is None else y 
-    assert len(x.shape) >= 2
-    assert len(y.shape) >= 2
-    return euclidean_distance(x[..., :, None, :], y[..., None, :, :])
-
-
-def euclidean_silhouette(X, z):
-    """
-    Computes the average Silhouette Coefficient with euclidean distance 
-    
-    More info:
-        - https://www.sciencedirect.com/science/article/pii/0377042787901257
-        - https://en.wikipedia.org/wiki/Silhouette_(clustering)
-    
-    Args:
-        X (array<m,n>): m x n float matrix with datapoints 
-        z (array<m>): m-length integer vector of cluster assignments
-    
-    Returns:
-        A scalar float with the silhouette score
-    """
-    X, z = np.asarray(X), np.asarray(z)
-    assert len(X.shape) == 2
-    assert len(z.shape) == 1
-    assert X.shape[0] == z.shape[0]
-    
-    # Compute average distances from each x to all other clusters
-    clusters = np.unique(z)
-    D = np.zeros((len(X), len(clusters)))
-    for i, ca in enumerate(clusters):
-        for j, cb in enumerate(clusters):
-            in_cluster_a = z == ca
-            in_cluster_b = z == cb
-            d = cross_euclidean_distance(X[in_cluster_a], X[in_cluster_b])
-            div = d.shape[1] - int(i == j)
-            D[in_cluster_a, j] = d.sum(axis=1) / np.clip(div, 1, None)
-    
-    # Intra distance 
-    a = D[np.arange(len(X)), z]
-    # Smallest inter distance 
-    inf_mask = np.where(z[:, None] == clusters[None], np.inf, 0)
-    b = (D + inf_mask).min(axis=1)
-    
-    return np.mean((b - a) / np.maximum(a, b))
